2cc65cf0a9b6530dde0c7662cc57a57de7c90d47
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2024-06-05 16:52:08 +0530

    updated utils

--- src/app/util.ts
+++ src/app/util.ts
@@ -1,10 +1,11 @@
-import { start } from "../api/index";
-import { Injector } from "@api/lib/injector";
-import { ProductServices } from "@api/services/products";
-import { AuthenticationServices } from "@api/services/authentication";
-import { ProductFeatureServices } from "@api/services/productFeatures";
-import { UserServices } from "@api/services/users";
-import { InjectedType } from "@api/types/types";
+// import { start } from "../api/index";
+// import { Injector } from "@api/lib/injector";
+// import { ProductServices } from "@api/services/products";
+// import { AuthenticationServices } from "@api/services/authentication";
+// import { ProductFeatureServices } from "@api/services/productFeatures";
+// import { UserServices } from "@api/services/users";
+// import { InjectedType } from "@api/types/types";
+import { formData } from "./type";
 
 // const returnType = (servicename: string) => {
 //   switch (servicename) {
@@ -21,9 +22,41 @@ import { InjectedType } from "@api/types/types";
 //   }
 // };
 
-export async function getservice<T>(serviceName: string):Promise<T> {
-  const service = Injector.get(serviceName);
-  if (service && Object.keys(service).length) return service as T;
-  await start();
-  return Injector.get(serviceName);
+// export async function getservice<T>(serviceName: string): Promise<T> {
+//   const service = Injector.get(serviceName);
+//   if (service && Object.keys(service).length) return service as T;
+//   await start();
+//   return Injector.get(serviceName);
+// }
+
+export function addValidationFunction(formData: formData) {
+  for (const form in formData) {
+    if (formData[form].validationArray?.length) {
+      const validation = [];
+      for (const validate of formData[form].validationArray) {
+        if (validate.method === "length") {
+          if (Array.isArray(validate.value)) {
+            validation.push(
+              (k: string) =>
+                k.length > validate.value[0] && k.length < validate.value[1]
+            );
+          } else {
+            validation.push((k: string) => k.length === validate.value);
+          }
+        }
+        if (validate.method === "includes") {
+          validation.push((k: string) => k.includes(validate.value));
+        }
+        if (validate.method === "dropDown") {
+          validation.push(
+            (k: string) =>
+              k &&
+              formData[form].dropDownValues.find((txt: string) => txt === k)
+          );
+        }
+      }
+      formData[form].validation = validation;
+    }
+  }
+  return formData;
 }
