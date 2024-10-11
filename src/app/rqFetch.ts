c273462ec798d8938fd5590925cf38e97db8f1f1
causality-3 <abinbabu003@gmail.com>
2024-10-11 17:32:57 +0530

    local fixes

--- src/app/rqFetch.ts
+++ src/app/rqFetch.ts
@@ -1,40 +1,14 @@
-import { apiCall } from "@/api/sevice";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+// import { apiCall } from "@/api/sevice";
+// import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-// Custom hook for fetching data
-const rqFetch = (
-  queryKey: any,
-  apiPayload: [string, string, ...any],
-  setrqData?: any
-) => {
-  const queryClient = useQueryClient();
-  const cachedrqData = queryClient.getQueryData(queryKey);
-  console.log("LALAN", queryKey, cachedrqData);
-
-  if (cachedrqData) {
-    return setrqData({ data: cachedrqData, isLoading: false, isError: false });
-  }
-
-  //   const dataApi = async () => {
-  //     return await apiCall(...apiPayload);
-  //   };
-
-  return setrqData(
-    useQuery({
-      queryFn: () => apiCall(...apiPayload),
-      queryKey,
-    })
-  );
-};
-
-export default rqFetch;
-// export function rqFetch(
+// // Custom hook for fetching data
+// const rqFetch = (
 //   queryKey: any,
 //   apiPayload: [string, string, ...any],
 //   setrqData?: any
-// ) {
+// ) => {
 //   const queryClient = useQueryClient();
 //   const cachedrqData = queryClient.getQueryData(queryKey);
 //   console.log("LALAN", queryKey, cachedrqData);
@@ -53,4 +27,30 @@ export default rqFetch;
 //       queryKey,
 //     })
 //   );
-// }
+// };
+
+// export default rqFetch;
+// // export function rqFetch(
+// //   queryKey: any,
+// //   apiPayload: [string, string, ...any],
+// //   setrqData?: any
+// // ) {
+// //   const queryClient = useQueryClient();
+// //   const cachedrqData = queryClient.getQueryData(queryKey);
+// //   console.log("LALAN", queryKey, cachedrqData);
+
+// //   if (cachedrqData) {
+// //     return setrqData({ data: cachedrqData, isLoading: false, isError: false });
+// //   }
+
+// //   //   const dataApi = async () => {
+// //   //     return await apiCall(...apiPayload);
+// //   //   };
+
+// //   return setrqData(
+// //     useQuery({
+// //       queryFn: () => apiCall(...apiPayload),
+// //       queryKey,
+// //     })
+// //   );
+// // }
